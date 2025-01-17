02568e815a6da9fc7710d7cae393ffdfa9651320
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-17 12:31:43 +0000

    variables

---
+++ basics01/01_variables.js
@@ -0,0 +1,9 @@
+const accountId= 122333
+let accountEmail = "vvv@123"
+var accountPassword = "12345"
+accountCity = "Mohali"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountEmail, accountId,accountPassword,accountCity,accountState])
