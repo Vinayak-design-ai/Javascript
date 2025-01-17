e7716062ae5074cb8eb767d5bf35500ee5c7f2af
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-17 12:56:48 +0000

    converssion of data types

---
+++ basics01/03_operationConversion.js
@@ -0,0 +1,19 @@
+let score = "33abc";
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);// if u add alphabets with number then print the following u eill get Nan (not a)
+
+// "33" =>33
+// "33abc" => NaN (not a number)
+// true =>1
+// false => 0
+let isLoggedIn = "vvvv"
+
+let isBoolean = Boolean(isLoggedIn)
+console.log(isBoolean);
+
+// in case of conversion in boolean
+// 1=>true , 0=>False
+// ""=>false (empty block)
+//"vvvvv" => true 
