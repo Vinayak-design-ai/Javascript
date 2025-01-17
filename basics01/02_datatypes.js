63f4860a7ca992bb9fe45a5fa4bad2d9bf6d586a
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-17 12:30:46 +0000

    data types

---
+++ basics01/02_datatypes.js
@@ -0,0 +1,22 @@
+let name = "Vinayak";
+let age = 20;
+let isLoggedIn = true;
+
+/* Data types
+Number 2 to the power 53
+string ""(Double commas are preffered)
+bigint (used rare)
+boolean true/false
+null => standalone value & different from 0(IS AN OBJECT)
+undefined =>
+symbol => Unique
+
+MOST USED AND IMPORTANT 
+object 
+
+*/
+
+
+console.log(typeof age);
+console.log(typeof null);// result is object
+console.log(typeof undefined);// result is undefined
