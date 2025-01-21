95f93c5d2de5d3acc19f02543fd9728b2f949ecb
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-21 13:18:53 +0000

    arrays in js

---
+++ basics02/01_arrays.js
@@ -0,0 +1,33 @@
+//// ARRAYS
+
+let arr= [1, 2, 3, 4, 5]
+let heroes = ["Ironman","Thor", "Captain", "Hulk"]
+let arr2 = new Array( 0 , 1 , 2 , 3)
+let arr3=  new Array("Hello", "My", "Name", "is", "Vinayak")
+console.log(arr);
+console.log (typeof arr);
+console.log(heroes[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
