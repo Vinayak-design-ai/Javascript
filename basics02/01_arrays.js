31936d99884c41bcfdbdd4aaa2207cbbb108fd4e
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-22 13:08:58 +0000

    arrays part 1

--- basics02/01_arrays.js
+++ basics02/01_arrays.js
@@ -1,12 +1,45 @@
 //// ARRAYS
 
-let arr= [1, 2, 3, 4, 5]
-let heroes = ["Ironman","Thor", "Captain", "Hulk"]
-let arr2 = new Array( 0 , 1 , 2 , 3)
-let arr3=  new Array("Hello", "My", "Name", "is", "Vinayak")
+const arr = [ 1,2, 3, 4, 5]
+const heroes = ["Ironman" , "Thor", "Captain America"]
+
+const arr2 = new Array("hello", "world" , 1 , 2)
+
+
+/// METHODS IN ARRAY
+// arr.push(6 ) // adds a new number in the end
+// arr.push( "YOo bitch")
+// arr.pop()// removes the last number from the array
+// arr.unshift(9, "Hiii") // adds the number in the beginning
+// arr.shift() // removes the number from the beginning
+
+// console.log(arr.includes(5)); //  returns boolean i.e. true or false
+// console.log(arr.indexOf(11)); // tells the index . if the number doesnt exist , it gives result as -1
+// console.log(arr.indexOf(1));
+// console.log(heroes.includes("Ironman", "Thor"));
+// const newArr = arr.join()// concatinates the different elements and returns a string in result.
+// const newArr1 = arr.join('') // removes the space
+// const newArr2 = arr.join('-')// adds the hash bw nums
+// console.log(arr);
+// console.log(newArr);
+// console.log(newArr1);
+// console.log(newArr2);
+
+const myN1 = arr.slice(1,3)
+console.log("A",myN1);
 console.log(arr);
-console.log (typeof arr);
-console.log(heroes[0]);
+
+
+const myN2 = arr ;
+console.log("B", myN2);
+
+const myN3 = arr.splice(1,3)
+console.log("C", myN3);
+console.log(arr);
+
+
+
+
 
 
 
