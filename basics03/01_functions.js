bc23df7120efbd90cd22cb8b0fa8f9adb57b3f74
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-27 13:11:00 +0000

    functionss

---
+++ basics03/01_functions.js
@@ -0,0 +1,35 @@
+function sayMyname(){
+    console.log("V");
+    console.log("I");
+    console.log("N");
+    console.log("A");
+    console.log("Y");
+    console.log("A");
+    console.log("K");  
+}
+// sayMyname()
+// sayMyname
+
+function addTwoNums(number1 , number2) { // (number1 , number2) these are called parameters basically they are a framework for the arguments
+    // let result = number1 + number2
+    // console.log("Vinayak") // one method to return 
+    return number1 + number2  // seconf method to return
+      
+}
+// const result = addTwoNums (6 , 63)
+// console.log("Result : ", result);
+
+// addTwoNums(9 , 99) // (9 , 99) these are called arguments that are put in in thr parameters for the result basiclly type of input 
+// addTwoNums(9 , "99")
+// addTwoNums(9 , null)
+
+function userLoggedIn(username = "Vinay"){  // with this it has a pre defined value and if we have no input then it will return the defined value and if any input is provided then the pre defined value will be over written
+    if (!username){
+        // if (username === undefined){
+        console.log("Please enter valid username");
+        return
+
+    }
+    return `${username} Just Logged In`
+}
+console.log(userLoggedIn())
