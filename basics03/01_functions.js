f616def2fd65a92b577730801146852b24e91602
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-28 11:19:18 +0000

    function addition

--- basics03/01_functions.js
+++ basics03/01_functions.js
@@ -39,4 +39,40 @@ function calculateCartPrice(val1 , val2 ,...num1){
     return num1
 
 }
-console.log(calculateCartPrice(100 , 200 , 300 , 1000 , 2000));
+// console.log(calculateCartPrice(100 , 200 , 300 , 1000 , 2000));
+
+const user = {
+    username : "Vinayak" ,
+    price : 1999 ,   
+}
+
+// function usingObject(anyobject) { // gave the input anyobject to show that any name of obj can be used .
+//     console.log(`The Username is ${anyobject.username} and the Price is ${anyobject.price}`);
+    
+// }
+// usingObject(user) // here we created an obj earlier and then put the name of the obj in the function execution.
+// usingObject ({ // here we directly gave the values in the execution of the function 
+//     username : "daman",
+//     price : 9999
+// })
+
+const myArray = [200 , 400 , 500 , 600]
+function returnSecondValue (anyArray){
+    return anyArray[2] 
+   
+    
+}
+// console.log(returnSecondValue(myArray))
+
+function addNums(num1 , num2){
+    console.log(num1 + num2);
+    return (`The Sum of the numbers is ${num1 + num2}`)
+}
+function minusNums( num1 , num2){
+    // console.log(num1 - num2);
+    if ((num1 + num2)/2 ===  0);
+    return(`Number ${num1 + num2} is an Even Number`)
+}
+console.log(minusNums(0, 21));
+
+
