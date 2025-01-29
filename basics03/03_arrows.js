b253cecf83c78c51a3e025a250d3e72b4dfd3388
Vinayak-design-ai <vinayak87652@gmail.com>
2025-01-29 12:38:44 +0000

    pange with this method

--- basics03/03_arrows.js
+++ basics03/03_arrows.js
@@ -58,5 +58,55 @@ const user =  {
 // IMPLICIT RETURN of FUNCTION 
 // const addTwo = (num1 , num2) => num1 + num2 // this works as well
 // const addTwo = (num1 , num2) => (num1 + num2) // this is another method 
-const addTwo = (num1 , num2) => ({username : "Vinayak"})
-console.log(addTwo(55, 88));
+// const addTwo = (num1 , num2) => ({username : "Vinayak"})
+// console.log(addTwo(55, 88));
+
+// function yourAge (userAge){
+//     this.userAge = 10
+//     function ageInYears (){
+//         console.log(`your age is ${this.userAge}`);
+        
+//     }
+//     ageInYears();
+    
+// console.log(`YOUR AGE IS ${userAge}`);
+
+    
+
+//     }
+    
+// yourAge(1)
+
+
+
+// const instaUser= {
+//     username : "Vinayak",
+//     password : "123abc",
+
+//     messageForUser: function() {
+//         console.log(`Welcome onBoard my friend ${this.username}`)
+//         // console.log(this);
+//         ;
+//     }
+//    }
+// //  console.log(instaUser)
+//  instaUser.messageForUser()
+ // ++++++++  ++++++ LEARNT SOMETHING REALLY NICE ++++++++++++++++ IN NEXT LINE
+ // IN CASE OF FUNCTION
+ // in function you have to define the value of this along with the key of which you want the value like in the case below 
+ // if i dont define (this.firstname = firstname + lastname) , then it wont have any value to take the input from in case of function.
+ // as in function we have to define bczit doesnt know from which argument we have to take value of this 
+// IN CASE OF OBJECT ++++++
+// i dont have to define this as it will directly take value from the keys there 
+function instaUser(firstName , lastName){ 
+    firstName = "Daman"
+    lastName = "Preet Kaur"
+    this.firstName = firstName + lastName
+    function register (name){
+        console.log(`Instagram welcomes you @ ${this.firstName}`);
+        
+    }register()
+    console.log(`Welcome to our website @ ${this.firstName}`);
+    
+}
+instaUser()
