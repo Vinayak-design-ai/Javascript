4e8428d83a40d0b8843cf9eb924bf532661e5f91
Vinayak-design-ai <vinayak87652@gmail.com>
2025-02-02 15:55:39 +0000

    basics of map and chaining of maps

--- iterations_05/seven.js
+++ iterations_05/seven.js
@@ -4,9 +4,19 @@
 const numsAdd = [ 1,2,3,4,5,6,7,8,9,10]
 const myNums = (numsAdd.map( (nums ) =>{
     if (nums%2 !== 0 ) {
-       return console.log(`${nums} is an odd number`);
+    //    return console.log(`${nums} is an odd number `);
         
     } else ;
-    return `${nums} is an even number `} ))
-console.log(myNums);
+    // console.log(`${nums} is an even number `);
+    } ))
+// console.log(myNums);
+
+const nums= [1,2,3,4,5,6,7,8,9,10]
+const addNums = nums
+        .map( (num) =>  num *10 ) // this is called as CHAINING of MAPS 
+        .map( (num) => num + 1 )
+        .filter ( (num) => num >=50 ) // can also add filters along with chaining of maps .
+        // .map( (num) => num / 10 )
+        // .map( (num) => num + 0.9 )
+console.log(addNums);
 
