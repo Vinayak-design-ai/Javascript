3537db0e02e38a476d496ac93a259fd04792963a
Vinayak-design-ai <vinayak87652@gmail.com>
2025-02-12 13:24:47 +0000

    basics_of_promise_fetch

---
+++ 09_advance_one/promise.js
@@ -0,0 +1,104 @@
+// // ++++ This Promise was Written in 2 PArts
+// const promiseOne = new Promise( function (resolve , reject) {
+//     // Do an async task
+//     // DB calls , cryptography , network
+//     setTimeout(function(){
+//         console.log("Async Task is Complete");
+//            resolve()
+//     }, 1000)
+ 
+// } )
+
+// promiseOne.then (function (){
+//     console.log("Promise Consumed");
+// })
+
+// // ++++ Writing Promise In Single Line 
+// new Promise( function(resolve , reject){
+//     setTimeout(function(){
+//         console.log("Async Task 2");
+//         resolve()
+//     } , 1000)
+// }).then(function(){
+//     console.log("Async Task 2 Completed ");
+    
+// })
+
+// const promiseThree = new Promise(function(resolve , reject){
+//     setTimeout(function(){
+//         resolve({username : "Vinayak" , mail : "hii@123.com"})
+//     } , 1000)
+// })
+// promiseThree.then(function(obj){
+//     console.log(obj);
+// })
+
+// const promiseFour = new Promise((resolve , reject) => {
+//     setTimeout(() => {
+//         let error = true ;
+//         if (!error) {
+//             resolve({username : "Hii@123" , password : "1234"})
+//         } else {
+//             reject("ERROR FOUND : Something went wrong")
+//         }
+//     } ,1000 )
+// } )
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.username
+// })
+// .then((username) => {
+//     console.log(username);
+    
+// } )
+// .catch((error) => {
+//     console.log(error);
+// })
+// .finally ( () => {console.log("The Promise is either Resolved or Rejected");
+// })
+
+const promiseFive = new Promise (function (resolve , reject) {
+    setTimeout(() => {
+        let error = true ;
+        if (!error) {
+            resolve({username : "JavaScript" , password : "1234"})
+        } else {
+            reject("ERROR FOUND : JS went wrong")
+        }
+    } ,1000 )
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+// async function getAllUsers (){
+//    try {
+//         const response = await fetch ('https://jsonplaceholder.typicode.com/users')
+//         const data = awaitresponse.json()
+//          console.log(data);
+//    } catch (error) {
+//         console.log("E :" , error);
+        
+//    }
+    
+// }
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()   
+} )
+.then((data) => {
+    console.log(data);
+} )
+.catch((error) => console.log(error));
+ 
